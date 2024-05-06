ac9033d2cb688c43b68a4b40811a847dc015b773
samtiagovisbal16 <santiago.visbal@cecar.edu.co>
2024-05-06 11:08:50 -0500

    Agregue unos ejemplos

---
+++ js/utilidades.js
@@ -0,0 +1,32 @@
+var directorioImagenes = '../recursos/imagenes/'
+
+function crearNodo(tipoNodo) {
+
+    var nodo = document.createElement(tipoNodo)
+    return nodo
+
+}
+
+function crearNodoConTexto(tipoNodo, textoNodo){
+    var nodo = document.createElement(tipoNodo)
+    var textoNodo = document.createTextNode(textoNodo)
+    nodo.appendChild(textoNodo)
+    return nodo
+
+}
+
+function crearNodoImagen(nombreImagen, alt) {
+    var nodo = crearNodo("img")
+    nodo.src = directorioImagenes + nombreImagen
+    nodo.alt = alt
+    return nodo
+
+}
+
+function adicionarNodoABody(nodo) {
+    document.body.appendChild(nodo)
+}
+
+function adicionarNodoAContenedor(nodo,contenedor) {
+    contenedor.appendChild(nodo)
+}
